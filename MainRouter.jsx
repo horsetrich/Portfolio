3c695aad4c77849edaf962e63fa4df75819aabd7
horsetrich <matthewkool12@gmail.com>
2024-09-24 17:20:29 -0400

    change

--- MainRouter.jsx
+++ MainRouter.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
-import About from './src/about'
+import About from './src/About'
 import Contact from './src/Contact'
 import Education from './src/education'
 import Project from './src/Project'

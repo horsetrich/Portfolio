690e51a3dc46f2bb46ede15bd7413a2a0c40d70d
horsetrich <matthewkool12@gmail.com>
2024-09-24 18:09:40 -0400

    go back

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import MainRouter from '../MainRouter';
-import {Link} from 'react-router-dom';
 
 
 const App = () => {

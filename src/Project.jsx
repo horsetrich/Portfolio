3c9e37057ccaebe16108173ac0db50ed51e47a2f
horsetrich <matthewkool12@gmail.com>
2024-09-24 19:58:33 -0400

    added project

--- src/Project.jsx
+++ src/Project.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ProjectOne from './Project1.png';
 import ProjectTwo from './Project2.png';
+import ProjectThree from './Project3.png';
 
 const projects = [
     { id: 1, title: 'Project 1', description: 'I created a 3D model of this cat character with animations', image: ProjectOne },
     { id: 2, title: 'Project 2', description: <a href="https://tmcausla.itch.io/season-of-the-witch">Worked on a small game. I was in charge of creating the enemies, boss, health, items, melee and spells</a>, image: ProjectTwo},
-    { id: 3, title: 'Project 3', description: 'Something', iamge: 'Nothing' }
+    { id: 3, title: 'Project 3', description: 'You are looking at it', image: ProjectThree }
 ];
 
 const Project = () => {
